e88dcfac4d7c5b9905929ad444cc69c95ae8cb60
aashish0770 <ssuzzan81@gmail.com>
2026-01-29 13:18:52 +0200

    frontend set up TS and tailwind with custome styling and color format

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center flex-col gap-2 bg-linear-to-r from-cyan-500 to-blue-200">
+      <h1 className=" ">
+        Personalized Study Planner
+      </h1>
+      <h3 className=" ">
+        Customize your study plan and track your progress{" "}
+      </h3>
+    </div>
+  );
+}
+
+export default App;
