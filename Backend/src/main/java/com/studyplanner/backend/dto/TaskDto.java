4e7a5d42c41f98e1ef5504fc75f7fa2afb140057
aashish0770 <ssuzzan81@gmail.com>
2026-02-13 20:07:08 +0200

    task mapper to send data to back and forth

--- Backend/src/main/java/com/studyplanner/backend/dto/TaskDto.java
+++ Backend/src/main/java/com/studyplanner/backend/dto/TaskDto.java
@@ -1,4 +1,31 @@
 package com.studyplanner.backend.dto;
 
+import java.time.LocalDateTime;
+
+import com.studyplanner.backend.entity.Task;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class TaskDto {
+
+    private Long taskId;
+    @NotNull(message = "User ID is required")
+    private Long userId;
+    private String taskName;
+    private String taskDescription;
+    private LocalDateTime taskDeadline;
+    private Task.Priority priority;
+    private Task.Status status;
+    private boolean completed;
 }
