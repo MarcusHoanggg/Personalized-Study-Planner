6ba94bd1cd9c7d18fb8b36d5d05b8b5969959a9e
aashish0770 <ssuzzan81@gmail.com>
2026-02-14 20:47:35 +0200

    error fix

--- Backend/src/main/java/com/studyplanner/backend/repository/ReminderRepository.java
+++ Backend/src/main/java/com/studyplanner/backend/repository/ReminderRepository.java
@@ -14,8 +14,9 @@ public interface ReminderRepository extends JpaRepository<Reminder, Long> {
     // Used by the scheduler, unsent reminders are sent by the scheduler
 
     @Query("SELECT r FROM Reminder r " +
-            "JOIN FETCH r.task t" +
-            "JOIN FETCH t.user u" + "WHERE r.reminderSent = false" +
+            "JOIN FETCH r.task t " +
+            "JOIN FETCH t.user u " +
+            "WHERE r.reminderSent = false " +
             "AND r.reminderDate BETWEEN :from AND :to")
     List<Reminder> findUnsentRemindersInWindow(
             @Param("from") LocalDateTime from,
@@ -28,9 +29,9 @@ public interface ReminderRepository extends JpaRepository<Reminder, Long> {
     List<Reminder> findByTaskId(Long taskId);
 
     // Find all unsent in-app notifications for a user
-    @Query("SELECT r FROM Reminder r" +
-            "JOIN FETCH r.task t" +
-            "WHERE t.user.id = :userId" +
+    @Query("SELECT r FROM Reminder r " +
+            "JOIN FETCH r.task t " +
+            "WHERE t.user.id = :userId " +
             "AND r.reminderSent = false")
     List<Reminder> findUnsentRemindersByUserId(@Param("userId") Long userId);
 
